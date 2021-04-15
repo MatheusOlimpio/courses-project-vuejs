e0d0664b85e0af5f22ee0ff086681dc6eca3cbed
Matheus Olimpio de Oliveira <mah.oliver_crz@hotmail.com>
2021-04-15 20:52:30 -0300

    Create a mixin fetchApi

---
+++ src/mixins/fetchData.js
@@ -0,0 +1,23 @@
+export default {
+  data() {
+    return {
+      api: {},
+      loading: true
+    }
+  },
+  methods: {
+    fetchData(url) {
+      this.loading = true;
+      this.api = null;
+      fetch(`http://localhost:3000${url}`)
+      .then(response => response.json())
+      .then(data => {
+        setTimeout(() => {
+          this.api = data;
+          this.loading = false;
+        }, 1500);
+
+      }); 
+    }
+  },
+}
